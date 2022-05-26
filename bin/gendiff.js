a2ba41c8efb765e8c71ae8d9b0aad741bbee8f26
Elena <lekolupaeva1201@gmail.com>
2022-05-26 19:26:35 +0200

    feat(gendiff.js) : add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ import { program } from 'commander';
 
 program
   .version('1.0')  
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   
   program.parse();
