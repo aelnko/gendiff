e71c7401ef162e1ca86fba077319c0b1e3f36180
Elena <lekolupaeva1201@gmail.com>
2022-06-01 16:09:21 +0200

    feat(tests): edit tests for gendiff

--- README.md
+++ README.md
@@ -15,3 +15,15 @@ gendiff file1.json file2.json
 gendiff file1.yaml file2.yml
 ```
 [![Install](https://asciinema.org/a/BZ9WweyModYl3wgHSqi86hwQK.svg)](https://asciinema.org/a/BZ9WweyModYl3wgHSqi86hwQK)
+## Рекурсивное сравнение (JSON)
+Для запуска выполните комнаду:
+```
+gendiff file1r.json file2r.json
+```
+[![Install](https://asciinema.org/a/X4oFZkInkoHjd0EErBAjy7L52.svg)](https://asciinema.org/a/X4oFZkInkoHjd0EErBAjy7L52)
+## Рекурсивное сравнение (YAML)
+Для запуска выполните команду
+``` 
+gendiff file1r.yaml file2r.yml
+```
+[![Install](https://asciinema.org/a/DzCntK1mUtQlBnu03VOWqONSD.svg)](https://asciinema.org/a/DzCntK1mUtQlBnu03VOWqONSD)
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,56 +14,64 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const file1 = fs.readFileSync(getFixturePath('file1.json'));
+const file1 = getFixturePath('file1.json');
 const file2 = getFixturePath('file2.json');
-const file1Yaml = getFixturePath('file1.yaml');
+const file1yml = getFixturePath('file1.yaml');
+const file2yml = getFixturePath('file2.yml');
 
-const expectedStylish = fs.readFileSync(getFixturePath('expected_stylish.txt'));
-
-const dataJson1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
-const dataYaml1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
-const objToTree = {
-  'group1': {
-    'baz': 'bas',
-    'foo': 'bar',
-    'nest': {
-      'key': 'value',
-    },
-  },
-};
-
-const obj2ToTree = {
-  'group1': {
-    'foo': 'bar',
-    'baz': 'bars',
-    'nest': 'str',
-  },
-};
-
-const expectedTree = '{\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n}';
+const expectedStylish = fs.readFileSync(getFixturePath('stylish.txt'), 'utf-8');
 
 test('gendiff', () => {
-  expect(gendiff(file1, file2)).toEqual(expectedStylish);
+  expect(gendiff(file1, file2)).toBe(expectedStylish);
+  expect(gendiff(file1yml, file2yml)).toBe(expectedStylish);
 });
 
-test('format', () => {
-  expect(getFileData(file1, '.json')).toEqual(dataJson1);
-  expect(getFileData(file1Yaml, '.yaml')).toEqual(dataYaml1);
-  expect(getFileData(file1Yaml, '.yml')).toEqual(dataYaml1);
-});
+// const dataJson1 = {
+//   host: 'hexlet.io',
+//   timeout: 50,
+//   proxy: '123.234.53.22',
+//   follow: false,
+// };
 
-test('formatter', () => {
-  expect(stringPrinting(buildTree(objToTree, obj2ToTree))).toEqual(expectedTree);
-});
+// const dataYaml1 = {
+//   host: 'hexlet.io',
+//   timeout: 50,
+//   proxy: '123.234.53.22',
+//   follow: false,
+// };
+
+// const objToTree = {
+//   'group1': {
+//     'baz': 'bas',
+//     'foo': 'bar',
+//     'nest': {
+//       'key': 'value',
+//     },
+//   },
+// };
+
+// const obj2ToTree = {
+//   'group1': {
+//     'foo': 'bar',
+//     'baz': 'bars',
+//     'nest': 'str',
+//   },
+// };
+
+// const expectedTree = '{\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n}';
+
+// test('gendiff', () => {
+//   expect(gendiff(file1, file2)).toEqual(expectedStylish);
+// });
+
+// test('format', () => {
+//   expect(getFileData(file1, '.json')).toEqual(dataJson1);
+//   expect(getFileData(file1Yaml, '.yaml')).toEqual(dataYaml1);
+//   expect(getFileData(file1Yaml, '.yml')).toEqual(dataYaml1);
+// });
+
+// test('formatter', () => {
+//   expect(stringPrinting(buildTree(objToTree, obj2ToTree))).toEqual(expectedTree);
+// });
+
+
